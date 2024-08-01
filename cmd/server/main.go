1f498c666ceee8c72ff0d49fe48adbb1c0d0a273
Jorge Sarkis <jorgesaicoski@gmail.com>
2024-08-01 20:32:35 -0300

    routing working

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
         "github.com/gin-gonic/gin"
         "github.com/JorgeSaicoski/go-todo-list/internal/db"
         "github.com/JorgeSaicoski/go-todo-list/internal/api"
+        "net/http"
 )
 
 func main() {
@@ -12,13 +13,16 @@ func main() {
         
 
         router := gin.Default()
+        router.LoadHTMLGlob("public/*")
 
         router.GET("/tasks", api.GetTasks)
         router.POST("/tasks", api.CreateTask)
-		router.GET("/", func(c *gin.Context) {
-			c.File("public/index.html")
-		})
+        router.GET("/", func(c *gin.Context) {
+                c.HTML(http.StatusOK, "index.tmpl", gin.H{
+                    "title": "Tasks",
+                })
+            })
 
 
-        router.Run(":8080")
+        router.Run(":8000")
 }
